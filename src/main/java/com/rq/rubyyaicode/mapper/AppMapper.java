9c09d940bf53ff1441da252df8744d7316482f29
rich0807 <3428773947@qq.com>
2025-11-16 15:18:01 +0800

    feat: 后端应用模块

---
+++ src/main/java/com/rq/rubyyaicode/mapper/AppMapper.java
@@ -0,0 +1,12 @@
+package com.rq.rubyyaicode.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.rq.rubyyaicode.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
